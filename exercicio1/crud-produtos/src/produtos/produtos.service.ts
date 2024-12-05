4e22d205d694ed45672824988f78667495c42ec6
allen080 <luanfelliipe8@gmail.com>
2024-12-05 20:00:09 -0300

    adding new methods on service

--- exercicio1/crud-produtos/src/produtos/produtos.service.ts
+++ exercicio1/crud-produtos/src/produtos/produtos.service.ts
@@ -18,4 +18,24 @@ export class ProdutosService {
     return this.produtoRepository.find();
   }
 
+  async findOne(id: number): Promise<Produto> {
+    const produto = await this.produtoRepository.findOne({where:{id}});
+    if (!produto) {
+      throw new NotFoundException(`Produto com ID ${id} não encontrado.`);
+    }
+    return produto;
+  }
+
+  async update(id: number, produto: Partial<Produto>): Promise<Produto> {
+    const produtoExistente = await this.findOne(id);
+    Object.assign(produtoExistente, produto);
+    return this.produtoRepository.save(produtoExistente);
+  }
+
+  async delete(id: number): Promise<void> {
+    const result = await this.produtoRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Produto com ID ${id} não encontrado.`);
+    }
+  }
 }
